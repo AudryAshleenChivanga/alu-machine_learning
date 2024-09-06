9e021c114631d51cfa7acbae9797ab54c0f6eb31
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-09-06 17:49:51 +0200

    Graphs showing correctly

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = np.arange(0, 11) ** 3
+
+plt.plot(y, 'r-')  # Plotting y axis as a solid red line 
+plt.xlim(0, 10)    # Set the x-axis range from 0 to 10
+
+plt.show()
 
