c7fcd62deb2a1adf1a477689ef8261c814f737b0
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-09-12 19:11:12 +0200

    3DONE

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def matrix_transpose(matrix):
+    """
+    Returns the transpose of a 2D matrix.
+
+    Args:
+    - matrix (list of lists): A 2D matrix represented as a list of lists.
+
+    Returns:
+    - list of lists: A new matrix which is the transpose of the input matrix.
+    """
+    # Using list comprehension to transpose the matrix
+    transposed = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+    return transposed
