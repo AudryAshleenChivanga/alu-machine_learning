02097dacb33979ca0171576cfc526526695b0403
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-09-13 01:51:02 +0200

    8 done multiplication

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+    This function performs matrix multiplication.
+"""
+
+
+def mat_mul(mat1, mat2):
+    """
+        This matrix perfoms matrix multiplication.
+        It assumes mat 1 and mat2 are 2D matrices
+        It must return a new matrix.
+    """
+    return(mat1 * mat2)
