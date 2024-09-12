b54f87b446cf4f4ceec5ed2a06a4b69032d565a7
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-09-12 18:34:15 +0200

    Calculating shape 3

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    """ This function calculates the shape of a matrix and returns it as a list of integers .
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix)
+        matrix=matrix[0]
+    return shape
