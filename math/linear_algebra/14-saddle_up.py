d45c28c5a5a5458713ef721b51407e81248fc10f
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-09-13 02:37:01 +0200

    Matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+    THis function performs matrix multiplication
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+        Multiplication
+    """
+    return np.matmul(mat1, mat2)
