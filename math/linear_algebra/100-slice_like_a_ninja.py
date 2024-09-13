e2ae9f9432818fc22d3c122ecdf3a73d9c531c51
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-09-13 18:46:08 +0200

    100 advanced

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
-
 """
-This script slices a matrix along specific axes (hahaha just like a ninja).
+Slice numpy arrays
 """
-
 import numpy as np
 
-def np_slice(matrix, axes={}):
-    """
-    Slices a matrix along specific axes.
+
+def np_slice(matrix, axes=None):
+    """Slices a matrix along specific axes
 
     Args:
-        matrix: The numpy.ndarray to slice.
-        axes: A dictionary where the key is an axis to slice along,
-              and the value is a tuple representing the slice to make along that axis.
+        matrix (numpy.ndarray): matrix to slice
+        axes (dict): dictionary where the key is an axis to slice along and
+                     the value is a tuple representing the slice to make along
+                     that axis
 
     Returns:
-        A new numpy.ndarray that has been sliced along the specified axes.
+        numpy.ndarray: the sliced matrix
     """
-    if not axes:  # If no axes are provided, return the matrix as is
+    if axes is None:
         return matrix[...]
-
     slices = []
-    for i in range(matrix.ndim):  # Iterate over each dimension of the matrix
+    for i in range(matrix.ndim):
         if i in axes:
-            slices.append(slice(*axes[i]))  # Append the slice specified for this axis
+            slices.append(slice(*axes[i]))
         else:
-            slices.append(slice(None))  # Append a default slice (':') if no slice is specified for this axis
-
-    return matrix[tuple(slices)]  # Return the sliced matrix
+            slices.append(slice(None))
+    return matrix[tuple(slices)]
+  
 mat1 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-print(np_slice(mat1, axes={1: (1, 3)}))  # Output sliced matrix
+print(np_slice(mat1, axes={1: (1, 3)}))
 print(mat1)
