ae55d86b57ede583e0f0135bc02f337f9b85e172
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-09-21 15:48:38 +0200

    file000

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -19,7 +19,8 @@ def determinant(matrix):
     TypeError: If the input is not a list of lists.
     ValueError: If the matrix is not square.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or not all(
+            isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     if len(matrix) == 1 and len(matrix[0]) == 0:
@@ -32,7 +33,7 @@ def determinant(matrix):
 
     if size == 1:
         return matrix[0][0]
-    
+
     determinant_value = 0
     for col in range(size):
         minor_matrix = [row[:col] + row[col + 1:] for row in matrix[1:]]
@@ -51,18 +52,18 @@ if __name__ == '__main__':
     mat5 = []
     mat6 = [[1, 2, 3], [4, 5, 6]]
 
-    print(determinant(mat0))  # Output: 1
-    print(determinant(mat1))  # Output: 5
-    print(determinant(mat2))  # Output: -2
-    print(determinant(mat3))  # Output: 0
-    print(determinant(mat4))  # Output: 192
+    print(determinant(mat0))
+    print(determinant(mat1))
+    print(determinant(mat2))
+    print(determinant(mat3))
+    print(determinant(mat4))
 
     try:
         determinant(mat5)
     except Exception as e:
-        print(e)  # Output: matrix must be a list of lists
+        print(e)
 
     try:
         determinant(mat6)
     except Exception as e:
-        print(e)  # Output: matrix must be a square matrix
+        print(e)
