e5359f7809b86b73f92727596943d5e3ae90a605
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-09-21 17:10:03 +0200

    Definiteness

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,54 +1,28 @@
 #!/usr/bin/env python3
+"""Function that calculates the definiteness of a matrix"""
 
 import numpy as np
 
-def definiteness(matrix):
-    if not isinstance(matrix, np.ndarray):
-        raise TypeError("matrix must be a numpy.ndarray")
 
+def definiteness(matrix):
+    """Function that calculates the definiteness of a matrix"""
+    if type(matrix) is not np.ndarray:
+        raise TypeError('matrix must be a numpy.ndarray')
     if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
         return None
-
-    if not np.allclose(matrix, matrix.T):
+    if matrix.shape[0] != matrix.shape[1]:
         return None
-
-    eigenvalues = np.linalg.eigvals(matrix)
-
-    if np.all(eigenvalues > 0):
-        return "Positive definite"
-    elif np.all(eigenvalues >= 0):
-        return "Positive semi-definite"
-    elif np.all(eigenvalues < 0):
+    transpose = np.transpose(matrix)
+    if not np.array_equal(transpose, matrix):
+        return None
+    ev, _ = np.linalg.eig(matrix)
+    if all(ev < 0):
         return "Negative definite"
-    elif np.all(eigenvalues <= 0):
+    if all(ev <= 0):
         return "Negative semi-definite"
-    elif np.any(eigenvalues > 0) and np.any(eigenvalues < 0):
-        return "Indefinite"
+    if all(ev > 0):
+        return "Positive definite"
+    if all(ev >= 0):
+        return "Positive semi-definite"
     else:
-        return None
-
-
-if __name__ == '__main__':
-    definiteness = __import__('5-definiteness').definiteness
-    import numpy as np
-
-    mat1 = np.array([[5, 1], [1, 1]])
-    mat2 = np.array([[2, 4], [4, 8]])
-    mat3 = np.array([[-1, 1], [1, -1]])
-    mat4 = np.array([[-2, 4], [4, -9]])
-    mat5 = np.array([[1, 2], [2, 1]])
-    mat6 = np.array([])
-    mat7 = np.array([[1, 2, 3], [4, 5, 6]])
-    mat8 = [[1, 2], [1, 2]]
-
-    print(definiteness(mat1))
-    print(definiteness(mat2))
-    print(definiteness(mat3))
-    print(definiteness(mat4))
-    print(definiteness(mat5))
-    print(definiteness(mat6))
-    print(definiteness(mat7))
-    try:
-        definiteness(mat8)
-    except Exception as e:
-        print(e)
+        return 'Indefinite'
