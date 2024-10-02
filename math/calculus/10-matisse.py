f4888baaea59610968bc2961cd29535cc88fc1a3
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-10-02 22:59:20 +0200

    File 10 corrected

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 """
-This module contains the function that calculates the summation of i squared.
+This module contains the function that calculates the polynomial derivative of a list of coefficients.
 """
 
 
-def summation_i_squared(n):
+def poly_derivative(poly):
     """
-    Calculates the summation of i squared.
+    Calculates the polynomial derivative of a list of coefficients.
     param: n: int
     return: int
     """
-    if type(n) is not int or n < 1:
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
-    return int((n * (n + 1) * (2 * n + 1)) / 6)
+    if len(poly) == 1:
+        return [0]
+    derivative = []
+    for i in range(1, len(poly)):
+        derivative.append(i * poly[i])
+    if derivative == [] or all(coeff == 0 for coeff in derivative):
+        return [0]
+    return derivative
