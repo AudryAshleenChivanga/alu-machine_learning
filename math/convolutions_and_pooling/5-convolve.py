a92831de23b3ac3378c544451c4f52d866c7c3a7
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-10-25 04:19:59 +0200

    Debugging 5

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
-"""Convolves images using multiple kernels"""
+"""Function that performs a convolution on images using multiple kernels"""
 
 import numpy as np
 
 
 def convolve(images, kernels, padding='same', stride=(1, 1)):
-    """Convolves images using multiple kernels
+    """Performs a convolution on images using multiple kernels
     Args:
-        images: `numpy.ndarray` (m, h, w, c) - images to convolve
-        kernels: `numpy.ndarray` (kh, kw, c, nc) - convolution kernels
-        padding: `tuple`, 'same', or 'valid'
-        stride: `tuple` (sh, sw) - stride of convolution
+        images: `numpy.ndarray` with shape (m, h, w)
+            containing multiple grayscale images
+            m: `int`, is the number of images
+            h: `int`, is the height in pixels of the images
+            w: `int`, is the width in pixels of the images
+            c: `int`, is the number of channels in the image
+        kernels: `numpy.ndarray` with shape (kh, kw, c, nc)
+            containing the kernel for the convolution
+            kh: `int`, is the height of the kernel
+            kw: `int`, is the width of the kernel
+            nc: `int`, is the number of kernels
+        padding: `tuple` of (ph, pw), ‘same’, or ‘valid’
+            if `tuple`:
+                ph: `int` is the padding for the height of the image
+                pw: `int` is the padding for the width of the image
+            if ‘same’, performs a same convolution
+            if ‘valid’, performs a valid convolution
+        stride is a tuple of (sh, sw)
+            sh: `int`, is the stride for the height of the image
+            sw: `int`, is the stride for the width of the image
     Returns:
-        `numpy.ndarray` with convolved images
+        output: `numpy.ndarray` containing the convolved images
     """
     m, h, w = images.shape[0], images.shape[1], images.shape[2]
     kh, kw, nc = kernels.shape[0], kernels.shape[1], kernels.shape[3]
@@ -21,13 +37,16 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
         pw = int(((w - 1) * sw + kw - w) / 2) + 1
         ph = int(((h - 1) * sh + kh - h) / 2) + 1
     elif padding == 'valid':
-        ph, pw = 0, 0
+        ph = 0
+        pw = 0
     else:
-        pw, ph = padding[1], padding[0]
-    nw, nh = int(((w - kw + 2 * pw) / sw) + 1), int(((h - kh + 2 * ph) / sh) + 1)
+        pw = padding[1]
+        ph = padding[0]
+    nw = int(((w - kw + (2 * pw)) / sw) + 1)
+    nh = int(((h - kh + (2 * ph)) / sh) + 1)
     convolved = np.zeros((m, nh, nw, nc))
-    imagesp = np.pad(images, pad_width=((0, 0),
-        (ph, ph), (pw, pw), (0, 0)),
+    npad = ((0, 0), (ph, ph), (pw, pw), (0, 0))
+    imagesp = np.pad(images, pad_width=npad,
                      mode='constant', constant_values=0)
     for i in range(nh):
         x = i * sh
