d519107d6039c221bda5118c6b7ecb817fb84379
AudryAshleenChivanga <a.chivanga@alustudent.com>
2025-01-24 15:36:26 +0200

    2

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,42 +1,17 @@
 #!/usr/bin/env python3
+
 import numpy as np
 
+Neuron = __import__('1-neuron').Neuron
 
-class Neuron:
-    """Defines a single neuron performing binary classification."""
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
 
-    def __init__(self, nx):
-        """
-        Initialize the Neuron.
-
-        Parameters:
-        nx (int): The number of input features to the neuron.
-
-        Raises:
-        TypeError: If nx is not an integer.
-        ValueError: If nx is less than 1.
-        """
-        if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
-        if nx < 1:
-            raise ValueError("nx must be positive")
-        
-        # Private instance attributes
-        self.__W = np.random.randn(1, nx)  # Random normal distribution
-        self.__b = 0  # Bias initialization
-        self.__A = 0  # Activated output initialization
-
-    @property
-    def W(self):
-        """Getter for weights vector."""
-        return self.__W
-
-    @property
-    def b(self):
-        """Getter for bias."""
-        return self.__b
-
-    @property
-    def A(self):
-        """Getter for activated output."""
-        return self.__A
+np.random.seed(0)
+neuron = Neuron(X.shape[0])
+print(neuron.W)
+print(neuron.b)
+print(neuron.A)
+neuron.A = 10
+print(neuron.A)
