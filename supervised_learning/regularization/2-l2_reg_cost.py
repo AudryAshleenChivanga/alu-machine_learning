f6cc107bc51c2763502a7568175cfd79282954a8
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2025-02-07 20:42:10 +0000

    Error debugging

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+    function def l2_reg_cost(cost): that
+    calculates the cost of a neural network
+    with L2 regularization:
+'''
+
+
+import tensorflow as tf
+
+
+def l2_reg_cost(cost):
+    '''
+        Args:
+            - cost: tensor containing the cost of the network
+
+        returns:
+            - tensor containing the cost of the network
+    '''
+    return cost + tf.losses.get_regularization_losses()
