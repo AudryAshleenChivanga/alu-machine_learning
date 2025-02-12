7160c4ba5fdc87dcf2930999c991b79be5390e8e
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2025-02-12 03:59:24 +0000

    Debugging

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -1,57 +1,23 @@
 #!/usr/bin/env python3
+'''
+    Function def update_variables_momentum(alpha, beta1, var, grad, v):
+    that updates a variable using the gradient descent with momentum
+    optimization algorithm:
+'''
 
-import matplotlib.pyplot as plt
-import numpy as np
-update_variables_momentum = __import__('5-momentum').update_variables_momentum
 
-def forward_prop(X, W, b):
-    Z = np.matmul(X, W) + b
-    A = 1 / (1 + np.exp(-Z))
-    return A
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    '''
+        Args:
+            - alpha is the learning rate
+            - beta1 is the momentum weight
+            - var is a numpy.ndarray containing the variable to be updated
+            - grad is a numpy.ndarray containing the gradient of var
+            - v is the previous first moment of var
 
-def calculate_grads(Y, A, W, b):
-    m = Y.shape[0]
-    dZ = A - Y
-    dW = np.matmul(X.T, dZ) / m
-    db = np.sum(dZ, axis=1, keepdims=True) / m
-    return dW, db
-
-def calculate_cost(Y, A):
-    m = Y.shape[0]
-    loss = - (Y * np.log(A) + (1 - Y) * np.log(1 - A))
-    cost = np.sum(loss) / m
-
-    return cost
-
-if __name__ == '__main__':
-    lib_train = np.load('../data/Binary_Train.npz')
-    X_3D, Y = lib_train['X'], lib_train['Y'].T
-    X = X_3D.reshape((X_3D.shape[0], -1))
-
-    nx = X.shape[1]
-    np.random.seed(0)
-    W = np.random.randn(nx, 1)
-    b = 0
-    dW_prev = np.zeros((nx, 1))
-    db_prev = 0
-    for i in range(1000):
-        A = forward_prop(X, W, b)
-        if not (i % 100):
-            cost = calculate_cost(Y, A)
-            print('Cost after {} iterations: {}'.format(i, cost))
-        dW, db = calculate_grads(Y, A, W, b)
-        W, dW_prev = update_variables_momentum(0.01, 0.9, W, dW, dW_prev)
-        b, db_prev = update_variables_momentum(0.01, 0.9, b, db, db_prev)
-    A = forward_prop(X, W, b)
-    cost = calculate_cost(Y, A)
-    print('Cost after {} iterations: {}'.format(1000, cost))
-
-    Y_pred = np.where(A >= 0.5, 1, 0)
-    fig = plt.figure(figsize=(10, 10))
-    for i in range(100):
-        fig.add_subplot(10, 10, i + 1)
-        plt.imshow(X_3D[i])
-        plt.title(str(Y_pred[i, 0]))
-        plt.axis('off')
-    plt.tight_layout()
-    plt.show()
+        Returns:
+            The updated variable and the new moment, respectively
+    '''
+    V = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * V
+    return var, V
--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -23,4 +23,4 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """
     s = beta2 * s + (1 - beta2) * np.power(grad, 2)
     var = var - alpha * grad / (np.sqrt(s) + epsilon)
-    return var, scode
+    return var, s
