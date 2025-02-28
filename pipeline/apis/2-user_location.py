83d0b9bf6dd40569c0ec7bb3ae22b69c3d301d04
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2025-02-28 21:59:29 +0000

    File2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,48 +1,25 @@
 #!/usr/bin/env python3
 
-import sys
+
+""" Return list of ships"""
+
 import requests
+import sys
 import time
 
-def get_user_location(api_url):
-    """
-    Fetches and prints the location of a GitHub user.
-
-    Args:
-        api_url (str): The full GitHub API URL of the user.
-
-    Returns:
-        None
-    """
-    try:
-        response = requests.get(api_url)
-        
-        # Handle rate limit exceeded
-        if response.status_code == 403:
-            reset_time = int(response.headers.get("X-RateLimit-Reset", time.time()))
-            minutes_until_reset = max(0, (reset_time - time.time()) // 60)
-            print("Reset in {} min".format(int(minutes_until_reset)))
-            return
-        
-        # Handle user not found
-        if response.status_code == 404:
-            print("Not found")
-            return
-        
-        # Handle successful response
-        if response.status_code == 200:
-            user_data = response.json()
-            location = user_data.get("location", "Not found")
-            print(location)
-            return
-        
-        # Handle other errors
-        print("Error: Unable to fetch data")
-    except requests.RequestException as e:
-        print("Request failed: {}".format(e))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./2-user_location.py <GitHub API URL>")
-    else:
-        get_user_location(sys.argv[1])
+    res = requests.get(sys.argv[1])
+
+    if res.status_code == 403:
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        current_time = int(time.time())
+        diff = (rate_limit - current_time) // 60
+        print("Reset in {} min".format(diff))
+        # get remaining rate
+
+    elif res.status_code == 404:
+        print("Not found")
+    elif res.status_code == 200:
+        res = res.json()
+        print(res['location'])
